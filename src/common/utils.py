74dbec56d13fbeab76b9383406e724de5ac20c1b
Liore Finkelstein <liore.finkelstein@genesys.com>
2023-12-11 17:48:29 +0200

    add utility method

---
+++ src/common/utils.py
@@ -0,0 +1,9 @@
+import os
+
+
+def printdir(dir):
+  filenames = os.listdir(dir)
+  for filename in filenames:
+    print(filename)  ## foo.txt
+    print(os.path.join(dir, filename)) ## dir/foo.txt (relative to current dir)
+    print(os.path.abspath(os.path.join(dir, filename))) ## /home/nick/dir/foo.txt
