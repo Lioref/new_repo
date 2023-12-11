e45eb1b8ed1c8acbf143e16add85eb4ed69b3af6
Liore Finkelstein <liore.finkelstein@genesys.com>
2023-12-11 17:49:07 +0200

    print -> pprint

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,4 +1,5 @@
 import os
+from pprint import pprint
 
 
 def printdir(dir):
@@ -6,4 +7,4 @@ def printdir(dir):
   for filename in filenames:
     print(filename)  ## foo.txt
     print(os.path.join(dir, filename)) ## dir/foo.txt (relative to current dir)
-    print(os.path.abspath(os.path.join(dir, filename))) ## /home/nick/dir/foo.txt
+    pprint(os.path.abspath(os.path.join(dir, filename))) ## /home/nick/dir/foo.txt
